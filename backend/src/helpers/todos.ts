78124c0e20b9c33cecfcda2f5a6f077775e3e6c2
Anthony <olympnetwork@gmail.com>
2022-11-10 08:31:56 +0100

    feat: Add todoAccess datalayer  and todos business logic

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,76 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
-import { TodoItem } from '../models/TodoItem'
-import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
-import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+import { TodosAccess } from "./todosAcess";
+// import { AttachmentUtils } from "./attachmentUtils";
+import { TodoItem } from "../models/TodoItem";
+import { CreateTodoRequest } from "../requests/CreateTodoRequest";
+import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
+import { createLogger } from "../utils/logger";
+import * as uuid from "uuid";
+import * as createError from "http-errors";
+// import { info } from "console";
 
 // TODO: Implement businessLogic
+const logger = createLogger("TodoService");
+
+// import { getUserId } from "../auth/utils";
+
+const todoAccess = new TodosAccess();
+
+export async function getAllTodoItems(): Promise<TodoItem[]> {
+  logger.info("Called todoAccess.getAllTodoItems");
+
+  return todoAccess.getAllTodoItems();
+}
+
+export async function createTodoItem(
+  createTodoRequest: CreateTodoRequest
+  //   jwtToken: string
+): Promise<TodoItem> {
+  const itemId = uuid.v4();
+  const userId = "1";
+  //   const userId = getUserId(jwtToken);
+  logger.info("Called todoAccess.createGroup");
+
+  return await todoAccess.createTodoItem({
+    todoId: itemId,
+    userId: userId,
+    name: createTodoRequest.name,
+    dueDate: createTodoRequest.dueDate,
+    createdAt: new Date().toISOString(),
+    done: false,
+    attachmentUrl: "https://test",
+  });
+}
+
+export async function getTodoItem(todoItem: string) {
+  logger.info(`Called todoAccess.getTodoItem with ${todoItem}`);
+  return todoAccess.getTodoItem(todoItem);
+}
+
+export async function updateTodoItem(
+  todoItem: string,
+  updatedFields: UpdateTodoRequest
+) {
+  logger.info(`Called todoAccess.TodoItemExists with ${todoItem}`);
+  //   Check if TodoItem exist
+  const todoExists = todoAccess.TodoItemExists(todoItem);
+
+  if (!todoExists) return createError(404, "Todo Item does not exist");
+
+  logger.info(
+    `Called todoAccess.updateTodoItem with ${todoItem} and ${updatedFields}`
+  );
+
+  return todoAccess.updateTodoItem(todoItem, updatedFields);
+}
+
+export async function deleteTodoItem(todoItem: string) {
+  logger.info(`Called todoAccess.TodoItemExists with ${todoItem}`);
+  //   Check if TodoItem exist
+  const todoExists = todoAccess.TodoItemExists(todoItem);
+
+  if (!todoExists) return createError(404, "Todo Item does not exist");
+
+  logger.info(`Called todoAccess.deleteTodoItem with ${todoItem} `);
+
+  return todoAccess.deleteTodoItem(todoItem);
+}
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,15 +1,17 @@
 import * as AWS from "aws-sdk";
-import * as AWSXRay from "aws-xray-sdk";
+// import * as AWSXRay from "aws-xray-sdk";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { createLogger } from "../utils/logger";
 import { TodoItem } from "../models/TodoItem";
 import { TodoUpdate } from "../models/TodoUpdate";
 
+const AWSXRay = require("aws-xray-sdk");
+
 const XAWS = AWSXRay.captureAWS(AWS);
 
 const logger = createLogger("TodosAccess");
 
-export class TodoAccess {
+export class TodosAccess {
   constructor(
     private readonly docClient: DocumentClient = createDynamoDBClient(),
     private readonly todosTable = process.env.TODOS_TABLE,
@@ -36,35 +38,19 @@ export class TodoAccess {
 
     const items = result.Items;
 
-    logger.info("completed querying all todos");
+    logger.info("completed querying all todos", items);
 
     return items as TodoItem[];
   }
 
-  async TodoItemExists(todoId: string): Promise<boolean> {
+  async getTodoItem(todoItemId: string): Promise<TodoItem> {
     logger.info("Initiated querying a todo item");
 
     const result = await this.docClient
       .get({
         TableName: this.todosTable,
         Key: {
-          id: todoId,
-        },
-      })
-      .promise();
-
-    logger.info("completed querying a todo item: ", result);
-    return !!result.Item;
-  }
-
-  async getTodoItem(todoId: string): Promise<TodoItem> {
-    logger.info("Initiated querying a todo item");
-
-    const result = await this.docClient
-      .get({
-        TableName: this.todosTable,
-        Key: {
-          id: todoId,
+          todoId: todoItemId,
         },
       })
       .promise();
@@ -85,21 +71,80 @@ export class TodoAccess {
       })
       .promise();
 
-    logger.info("completed creating a todo item");
+    logger.info(`created a todo item with id: ${todoItem.todoId}`);
 
     return todoItem;
+  }
+
+  async updateTodoItem(
+    todoItemId: string,
+    updatedFields: TodoUpdate
+  ): Promise<TodoItem | null> {
+    logger.info("Initiated updating a todo item");
+
+    const result = await this.docClient
+      .update({
+        TableName: this.todosTable,
+        Key: {
+          todoId: todoItemId,
+        },
+        UpdateExpression: "set name = :n, dueDate = :dd, done = :dn",
+        ExpressionAttributeValues: {
+          ":n": updatedFields.name,
+          ":dd": updatedFields.dueDate,
+          ":dn": updatedFields.done,
+        },
+      })
+      .promise();
+
+    const item = result.$response.data;
+
+    logger.info(`updated a todo item with id: ${todoItemId}`);
+
+    if (item) return item as TodoItem;
+
+    return null;
+  }
+
+  async deleteTodoItem(todoItemId: string): Promise<TodoItem | null> {
+    logger.info("Initiated deleting a todo item");
+
+    const result = await this.docClient
+      .delete({
+        TableName: this.todosTable,
+        Key: {
+          todoId: todoItemId,
+        },
+      })
+      .promise();
+
+    const item = result.$response.data;
+
+    logger.info(`deleted a todo item with id: ${todoItemId}`);
+
+    if (item) return item as TodoItem;
+
+    return null;
+  }
+
+  async TodoItemExists(todoItemId: string): Promise<boolean> {
+    logger.info("Initiated querying a todo item");
+
+    const result = await this.docClient
+      .get({
+        TableName: this.todosTable,
+        Key: {
+          todoId: todoItemId,
+        },
+      })
+      .promise();
+
+    logger.info("completed querying a todo item: ", result);
+    return !!result.Item;
   }
 }
 
 function createDynamoDBClient() {
-  // if (process.env.IS_OFFLINE) {
-  //   console.log("Creating a local DynamoDB instance");
-  //   return new XAWS.DynamoDB.DocumentClient({
-  //     region: "localhost",
-  //     endpoint: "http://localhost:8000",
-  //   });
-  // }
-
-  return new AWS.DynamoDB.DocumentClient();
-  // return new XAWS.DynamoDB.DocumentClient();
+  return new XAWS.DynamoDB.DocumentClient();
+  //   return new XAWS.DynamoDB.DocumentClient();
 }
